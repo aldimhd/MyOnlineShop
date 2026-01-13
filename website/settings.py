55e89b428c1d66dc1395f008f61d6f212c41961e
aldimhd <aldi.mhd07@gmail.com>
2026-01-13 13:38:59 +0700

    commit 1

--- website/settings.py
+++ website/settings.py
@@ -35,7 +35,6 @@ ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
 # APPLICATIONS
 # =========================
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
